2fc17d409312ef1eaf9d1fbb3e04db345de0eb88
maroze <grigorevaalla15@gmail.com>
2021-10-01 09:35:51 +0300

    first commit'

---
+++ Class12.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Class12
+    {
+        /// <summary>
+        /// Числитель
+        /// </summary>
+        int numerator;
+        /// <summary>
+        /// Знаменатель
+        /// </summary>
+        int denumerator;
+        public Class12(int a) { numerator = a; denumerator = 1; }
+        public Class12(int a, int b) { numerator = a; denumerator = b; }
+        public Class12(int a, int b, int c) { numerator = a * c + b; denumerator = c; }
+        public double ToFloat()
+        {
+            return (double)(numerator) / denumerator;
+        }
+    }
+}
